2e15a15148e295e9de83c562ca02273a1bf8a593
SergioArnaud <sercharnaud@gmail.com>
2022-01-29 01:05:02 -0600

    Update optimizer

--- optimizer.py
+++ optimizer.py
@@ -12,12 +12,19 @@ class ScheduledOptim():
     def step(self):
         "Step with the inner optimizer"
         self._update_learning_rate()
-        self._optimizer.step()
+        self.optimizer.step()
 
+    def state_dict(self):
+        """Returns the state of the scheduler as a :class:`dict`.
 
+        It contains an entry for every variable in self.__dict__ which
+        is not the optimizer.
+        """
+        return {key: value for key, value in self.__dict__.items() if key != 'optimizer'}
+        
     def zero_grad(self):
         "Zero out the gradients with the inner optimizer"
-        self._optimizer.zero_grad()
+        self.optimizer.zero_grad()
 
 
     def _get_lr_scale(self):
@@ -32,5 +39,5 @@ class ScheduledOptim():
         self.step_n += 1
         lr = self.lr_mul * self._get_lr_scale()
 
-        for param_group in self._optimizer.param_groups:
+        for param_group in self.optimizer.param_groups:
             param_group['lr'] = lr
